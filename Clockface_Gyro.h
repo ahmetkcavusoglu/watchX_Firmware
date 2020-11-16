168a7df488effb21b180168f27fe7a41d188f414
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-16 16:44:43 +0300

    Update and rename Clockface_Digital.h to Clockface_Gyro.h

---
+++ Clockface_Gyro.h
@@ -0,0 +1,14 @@
+#ifndef CLOCKFACE_GYRO_H
+#define CLOCKFACE_GYRO_H
+
+#include <Adafruit_GFX.h>
+#include "Clockface.h"
+
+class ClockfaceGyro : public Clockface {
+  public:
+    void draw(Adafruit_GFX* display) const;
+
+};
+
+void GyroGame(void);
+#endif
