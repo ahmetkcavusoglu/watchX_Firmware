4199c49a91c6aad276659a3347804dbab5cab617
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-19 00:26:48 +0300

    Update Menu_2.h

--- Menu_2.h
+++ Menu_2.h
@@ -1,21 +1,18 @@
-#ifndef MENU_2_H
-#define MENU_2_H
+#ifndef MENU_SETTINGS_24H_H
+#define MENU_SETTINGS_24H_H
 
 #include <Arduino.h>
 #include "Menu.h"
 
 class Settings24hMenu : public Menu {
-public:
-  Settings24hMenu();
-  bool update();
-  void button1();
-  void button2();
-  void button3();
-  void draw(Adafruit_GFX* display) const;
-  void update(uint8_t a, uint8_t b);
-  private:
-
-  uint8_t dots_x, blinky_x, die_timer;
+  public:
+    Settings24hMenu();
+    bool update();
+    void button1();
+    void button2();
+    void button3();
+    void draw(Adafruit_GFX* display) const;
+    uint8_t dots_xx, blinky_xx;
 };
 
 #endif
