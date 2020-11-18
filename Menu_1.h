93584e1a783bf1398ac90ab520c1d736580f633f
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-18 15:36:10 +0300

    Update and rename Clockface_Button.h to Menu_1.h

---
+++ Menu_1.h
@@ -0,0 +1,19 @@
+#ifndef MENU_HELLO_H
+#define MENU_HELLO_H
+
+#include <Arduino.h>
+#include "Menu.h"
+
+class SettingsMenu : public Menu {
+public:
+  SettingsMenu();
+  bool update();
+  void button1();
+  void button2();
+  void button3();
+  void draw(Adafruit_GFX* display) const;
+protected:
+
+};
+
+#endif
