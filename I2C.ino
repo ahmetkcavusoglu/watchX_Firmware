d3e0d6fd46b7e7c0909e04e72dbdd6a1f276fa36
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:10:57 +0300

    Update I2C.ino

--- I2C.ino
+++ I2C.ino
@@ -28,8 +28,8 @@ uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool se
   Wire.write(data, length);
   uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
   if (rcode) {
-    Serial.print(F("i2cWrite failed: "));
-    Serial.println(rcode);
+//    Serial.print(F("i2cWrite failed: "));
+//    Serial.println(rcode);
   }
   return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
 }
@@ -40,8 +40,8 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
   Wire.write(registerAddress);
   uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
   if (rcode) {
-    Serial.print(F("i2cRead failed: "));
-    Serial.println(rcode);
+//    Serial.print(F("i2cRead failed: "));
+//    Serial.println(rcode);
     return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
   }
   Wire.requestFrom(IMUAddress, nbytes, (uint8_t)true); // Send a repeated start and then release the bus after reading
@@ -54,7 +54,7 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
       if (Wire.available())
         data[i] = Wire.read();
       else {
-        Serial.println(F("i2cRead timeout"));
+//        Serial.println(F("i2cRead timeout"));
         return 5; // This error value is not already taken by endTransmission
       }
     }
