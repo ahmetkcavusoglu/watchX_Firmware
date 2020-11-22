43da2b0ed73a9c8c67c7e8e13d5548f0553ceb69
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:09:06 +0300

    Update Clockface.h

--- Clockface.h
+++ Clockface.h
@@ -7,7 +7,7 @@ class Clockface {
 public:
   virtual void update(uint8_t a, uint8_t b) {}
   virtual void draw(Adafruit_GFX* display) const = 0;
-    uint8_t dots_x, blinky_x, die_timer;
+
 };
 
 #endif
