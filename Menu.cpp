90fc5edc9dad3f630ebc6c546d98e1167d0817db
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:11:30 +0300

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -9,6 +9,7 @@
 #include "Menu_4.h"
 #include "Menu_Clockface.h"
 #include "State.h"
+#include "Clockface_Pacman.h"
 
 extern Menu* menu;
 
@@ -27,104 +28,117 @@ Menu_selection nextMenu;
 // because it was taking up way to much RAM.
 // So here I do manual virtual methods...
 bool Menu::update() {
-  switch(type) {
-    case MENU_HELLO:
-      return ((SettingsMenu*)this)->update();
-    case MENU_SETTINGS_24H:
-      return ((Settings24hMenu*)this)->update();
+  switch (type) {
     case MENU_START:
       return ((SettingsTimeMenu*)this)->update();
-//    case MENU_SETTINGS_DATE:
-//      return ((SettingsDateMenu*)this)->update();
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      return ((SettingsBrightnessMenu*)this)->update();
-  //  case MENU_SETTINGS_CLOCKFACE:
-    //  return ((SettingsClockfaceMenu*)this)->update();
+    case MENU_CANAVAR:
+      return ((Settings24hMenu*)this)->update();
+    case MENU_HELLO:
+      return ((SettingsMenu*)this)->update();
     case MENU_CLOCK:
       return ((ClockfaceMenu*)this)->update();
+    case MENU_AT:
+      return ((SettingsDateMenu*)this)->update();
+    case MENU_SETTINGS_CLOCKFACE:
+      return ((SettingsClockfaceMenu*)this)->update();
+    case MENU_PACMAN:
+      return ((ClockfacePacman*)this)->update();
+    case MENU_5:
+      return ((Menu5*)this)->update();
   }
 }
 
 void Menu::draw(Adafruit_GFX* display) const {
-  switch(type) {
-    case MENU_HELLO:
-      ((SettingsMenu*)this)->draw(display);
-      return;
-    case MENU_SETTINGS_24H:
-      ((Settings24hMenu*)this)->draw(display);
-      return;
+  switch (type) {
     case MENU_START:
       ((SettingsTimeMenu*)this)->draw(display);
       return;
-//    case MENU_SETTINGS_DATE:
-//      ((SettingsDateMenu*)this)->draw(display);
-//      return;
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      ((SettingsBrightnessMenu*)this)->draw(display);
-//      return;
-  //  case MENU_SETTINGS_CLOCKFACE:
-    //  ((SettingsClockfaceMenu*)this)->draw(display);
-    //  return;
+    case MENU_HELLO:
+      ((SettingsMenu*)this)->draw(display);
+      return;
+    case MENU_CANAVAR:
+      ((Settings24hMenu*)this)->draw(display);
+      return;
+
+    case MENU_AT:
+      ((SettingsDateMenu*)this)->draw(display);
+      return;
     case MENU_CLOCK:
       ((ClockfaceMenu*)this)->draw(display);
       return;
+    case MENU_SETTINGS_CLOCKFACE:
+      ((SettingsClockfaceMenu*)this)->draw(display);
+      return;
+    case MENU_PACMAN:
+      ((ClockfacePacman*)this)->draw(display);
+      return;
+    case MENU_5:
+      ((Menu5*)this)->draw(display);
+      return;
+
   }
 }
 
 void Menu::button1() {
-  switch(type) {
+  switch (type) {
     case MENU_HELLO:
       return ((SettingsMenu*)this)->button1();
-    case MENU_SETTINGS_24H:
+    case MENU_CANAVAR:
       return ((Settings24hMenu*)this)->button1();
     case MENU_START:
       return ((SettingsTimeMenu*)this)->button1();
-//    case MENU_SETTINGS_DATE:
-//      return ((SettingsDateMenu*)this)->button1();
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      return ((SettingsBrightnessMenu*)this)->button1();
-    //case MENU_SETTINGS_CLOCKFACE:
-      //return ((SettingsClockfaceMenu*)this)->button1();
     case MENU_CLOCK:
       return ((ClockfaceMenu*)this)->button1();
+    case MENU_AT:
+      return ((SettingsDateMenu*)this)->button1();
+    case MENU_SETTINGS_CLOCKFACE:
+      return ((SettingsClockfaceMenu*)this)->button1();
+    case MENU_PACMAN:
+      return ((ClockfacePacman*)this)->button1();
+    case MENU_5:
+      return ((Menu5*)this)->button1();
   }
 }
 
 void Menu::button3() {
-  switch(type) {
+  switch (type) {
     case MENU_HELLO:
       return ((SettingsMenu*)this)->button3();
-    case MENU_SETTINGS_24H:
+    case MENU_CANAVAR:
       return ((Settings24hMenu*)this)->button3();
     case MENU_START:
       return ((SettingsTimeMenu*)this)->button3();
-//    case MENU_SETTINGS_DATE:
-//      return ((SettingsDateMenu*)this)->button3();
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      return ((SettingsBrightnessMenu*)this)->button3();
-    //case MENU_SETTINGS_CLOCKFACE:
-      //return ((SettingsClockfaceMenu*)this)->button3();
     case MENU_CLOCK:
       return ((ClockfaceMenu*)this)->button3();
+    case MENU_AT:
+      return ((SettingsDateMenu*)this)->button3();
+    case MENU_SETTINGS_CLOCKFACE:
+      return ((SettingsClockfaceMenu*)this)->button3();
+    case MENU_PACMAN:
+      return ((ClockfacePacman*)this)->button3();
+    case MENU_5:
+      return ((Menu5*)this)->button3();
   }
 }
 
 void Menu::button2() {
-  switch(type) {
+  switch (type) {
     case MENU_HELLO:
       return ((SettingsMenu*)this)->button2();
-    case MENU_SETTINGS_24H:
+    case MENU_CANAVAR:
       return ((Settings24hMenu*)this)->button2();
     case MENU_START:
       return ((SettingsTimeMenu*)this)->button2();
-//    case MENU_SETTINGS_DATE:
-//      return ((SettingsDateMenu*)this)->button2();
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      return ((SettingsBrightnessMenu*)this)->button2();
-    //case MENU_SETTINGS_CLOCKFACE:
-      //return ((SettingsClockfaceMenu*)this)->button2();
     case MENU_CLOCK:
       return ((ClockfaceMenu*)this)->button2();
+    case MENU_AT:
+      return ((SettingsDateMenu*)this)->button2();
+    case MENU_SETTINGS_CLOCKFACE:
+      return ((SettingsClockfaceMenu*)this)->button2();
+    case MENU_PACMAN:
+      return ((ClockfacePacman*)this)->button2();
+    case MENU_5:
+      return ((Menu5*)this)->button2();
   }
 }
 
@@ -152,25 +166,30 @@ void updateMenuSelection() {
   }
 
   // Make the new one.
-  switch(nextMenu) {
+  switch (nextMenu) {
     case MENU_HELLO:
       menu = new SettingsMenu();
       break;
-    case MENU_SETTINGS_24H:
+    case MENU_CANAVAR:
       menu = new Settings24hMenu();
       break;
     case MENU_START:
       menu = new SettingsTimeMenu();
       break;
-//    case MENU_SETTINGS_DATE:
-//      menu = new SettingsDateMenu();
-//      break;
-//    case MENU_SETTINGS_BRIGHTNESS:
-//      menu = new SettingsBrightnessMenu();
-//      break;
     case MENU_CLOCK:
       menu = new ClockfaceMenu();
       break;
+    case MENU_AT:
+      menu = new SettingsDateMenu();
+      break;
+    case MENU_SETTINGS_CLOCKFACE:
+      menu = new SettingsClockfaceMenu();
+      break;
+    case MENU_PACMAN:
+      menu = new ClockfacePacman();
+      break;
+       case MENU_5:
+      menu = new Menu5();
+      break;
   }
 }
-
