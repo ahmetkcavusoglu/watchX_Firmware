80aea920d0925ea09c39c4aa67e1269127e96781
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:14:42 +0300

    Update Menu_3.h

--- Menu_3.h
+++ Menu_3.h
@@ -1,20 +1,18 @@
-#ifndef MENU_SETTINGS_BRIGHTNESS_H
-#define MENU_SETTINGS_BRIGHTNESS_H
+#ifndef MENU_AT_H
+#define MENU_AT_H
 
 #include <Arduino.h>
 #include "Menu.h"
 
-class SettingsBrightnessMenu : public Menu {
-public:
-  SettingsBrightnessMenu();
-  bool update();
-  void button1();
-  void button2();
-  void button3();
+class SettingsDateMenu : public Menu {
+  public:
+    SettingsDateMenu();
+    bool update();
+    void button1();
+    void button2();
+    void button3();
+    void draw(Adafruit_GFX* display) const;
 
-  void draw(Adafruit_GFX* display) const;
-private:
-  uint8_t selection;
 };
 
 #endif
