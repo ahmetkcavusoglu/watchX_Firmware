325e8a9a8ab816641eda5f7b80c7dc5c353948d8
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:10:14 +0300

    Update Clockface_Pacman.cpp

--- Clockface_Pacman.cpp
+++ Clockface_Pacman.cpp
@@ -3,7 +3,6 @@
 #include "Menu.h"
 #include "Menu_Clockface.h"
 #include "State.h"
-#include "Clockface.h"
 #include "Clockface_Pacman.h"
 
 #define WHITE 1
@@ -13,36 +12,47 @@
 
 #define DOTS_SPACE 10
 
-
-
 ClockfacePacman::ClockfacePacman()
-  : blinky_x(0)
+: Menu(MENU_PACMAN)
+  , blinky_x(0)
   , dots_x(0)
-  , die_timer(0)
+//  , die_timer(0)
 {}
 
-void ClockfacePacman::update(uint8_t a, uint8_t b) {
-  if (state.now.second() >= 5) {
+bool ClockfacePacman::update() {
+//  if (state.now.second() >= 5) {
     blinky_x = map(state.now.second(), 5, 60, WIDTH / 2 + 16, 12);
     dots_x = (dots_x + 2) % DOTS_SPACE;
-    die_timer = 0;
-  } else {
-    die_timer += 1;
-  }
+//    die_timer = 0;
+//  } else {
+//    die_timer += 1;
+//  }
 }
+void ClockfacePacman::button1() {
+  switchMenu(MENU_CANAVAR);
+}
+
+void ClockfacePacman::button3() {
+  switchMenu(MENU_CANAVAR);
+}
+
+void ClockfacePacman::button2() {
+  switchMenu(MENU_CANAVAR);
+}
+
 
 void ClockfacePacman::draw(Adafruit_GFX* display) const {
 
   display->setTextColor(WHITE);
   display->setTextSize(1);
-  display->setCursor(20, 0);
-  display->print(F("Now, open mBlock"));
-  display->setCursor(20, 10);
-  display->print(F("and start your"));
+  display->setCursor(10, 0);
+  display->print(F("Now, press button"));
+  display->setCursor(15, 10);
+  display->print(F("and connect"));
   display->setCursor(20, 20);
-  display->print(F("lessons :)"));
+  display->print(F("watchX to PC :)"));
 
-  // Pacman!
+//   Pacman!
   display->drawFastHLine(0, HEIGHT - 22, WIDTH, WHITE);
   display->drawFastHLine(0, HEIGHT - 3, WIDTH, WHITE);
 
@@ -57,4 +67,4 @@ void ClockfacePacman::draw(Adafruit_GFX* display) const {
   display->drawBitmap(WIDTH / 2, HEIGHT - 20, pacman_bmp + pac_frame, 16, 13, WHITE);
   display->drawBitmap(WIDTH / 2 - blinky_x, HEIGHT - 20, blinky_bmp + ghost_frame, 16, 14, WHITE);
 
-}
+  }
