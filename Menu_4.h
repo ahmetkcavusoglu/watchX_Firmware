6d5f8d4b842cfce79d24a492a54df248f098a543
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:15:20 +0300

    Update Menu_4.h

--- Menu_4.h
+++ Menu_4.h
@@ -5,15 +5,16 @@
 #include "Menu.h"
 
 class SettingsClockfaceMenu : public Menu {
-public:
-  SettingsClockfaceMenu();
-  bool update();
-  void button1();
-  void button2();
-  void button3();
-  void draw(Adafruit_GFX* display) const;
-private:
-  uint8_t selection;
+  public:
+    SettingsClockfaceMenu();
+    bool update();
+    void button1();
+    void button2();
+    void button3();
+    void draw(Adafruit_GFX* display) const;
+    uint8_t dots_xxxx, blinky_xxxx;
+  private:
+
 };
 
 #endif
