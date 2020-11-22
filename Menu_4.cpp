7ede6add6f8120cf7def84d792a7662bcd950988
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-22 04:15:02 +0300

    Update Menu_4.cpp

--- Menu_4.cpp
+++ Menu_4.cpp
@@ -1,9 +1,10 @@
-/* #include <Arduino.h>
+#include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "Menu_4.h"
 #include "Menu_Clockface.h"
 #include "State.h"
+#include "Clockface_Pacman.h"
 
 // Some graphics constants
 #define BLACK 0
@@ -12,68 +13,48 @@
 #define HEIGHT 64
 
 SettingsClockfaceMenu::SettingsClockfaceMenu()
-: Menu(MENU_SETTINGS_CLOCKFACE)
-, selection(0)
+  : Menu(MENU_SETTINGS_CLOCKFACE)
+  , blinky_xxxx(0)
+  , dots_xxxx(0)
+
 {}
 
 bool SettingsClockfaceMenu::update() {
-  return state.timeMinuteUpdated;
+//  if (state.now.second() >= 5) {
+    blinky_xxxx = map(state.now.second(), 5, 60, WIDTH / 2 + 16, 12);
+    dots_xxxx = (dots_xxxx + 2) % 10;
+//  }
 }
 
 void SettingsClockfaceMenu::button1() {
-  selection = (selection + 1) % (FACE_MAX+1);
+  switchMenu(MENU_CLOCK);
 }
 
 void SettingsClockfaceMenu::button3() {
-  selection = (selection - 1) % (FACE_MAX+1);
+  switchMenu(MENU_CLOCK);
 }
 
 void SettingsClockfaceMenu::button2() {
-  if (selection == FACE_MAX) {
-    switchMenu(MENU_SETTINGS);
-  } else {
-    state.enabled_faces ^= _BV(selection);
-    state.save();
-  }
+  switchMenu(MENU_CLOCK);
 }
-
+int xPos2 = 0;
 void SettingsClockfaceMenu::draw(Adafruit_GFX* display) const {
-  drawLayout(display, F("Clock"));
 
-  // Menu Items
-  for (uint8_t i = 0; i < FACE_MAX+1; ++i) {
-    display->setCursor(5, 13 + (i * 10));
-    if (selection == i) {
-      display->setTextColor(BLACK, WHITE);
-      display->fillRect(
-        0    , 12 + (i * 10),
-        WIDTH, 9,
-        WHITE);
-    } else {
-      display->setTextColor(WHITE);
-    }
-    if (i == FACE_MAX) {
-      display->print(F("Back"));
-    } else {
-      if (state.enabled_faces & _BV(i)) {
-        display->print('*');
-      } else {
-        display->print(' ');
-      }
-      switch (i) {
-      case FACE_PONG:
-        display->print(F(" Pong"));
-       break;
-     // case FACE_DIGITAL:
-       // display->print(F(" Digital"));
-      //  break;
-//      case FACE_PACMAN:
-//        display->print(F(" Pacman"));
-//        break;
-     // case FACE_TETRIS:
-       // display->print(F(" Tetris"));
-      //  break;
-      }
-    }
+  uint8_t pac_framee = dots_xxxx < 2 ? 52 : (dots_xxxx < 6 ? 26 : 0);
+  uint8_t ghost_framee = dots_xxxx < 5 ? 0 : 28;
+
+  // Approach
+
+  display->drawBitmap(xPos2, HEIGHT - 35, pacman_bmp + pac_framee, 16, 13, WHITE);
+  display->drawBitmap(xPos2 - blinky_xxxx + 50, HEIGHT - 35, blinky_bmp + ghost_framee, 16, 14, WHITE);
+  xPos2 += 1;
+  if (xPos2 > 127) {
+    xPos2 = 0;
   }
-} */
+  display->setTextColor(WHITE);
+  display->setTextSize(1);
+  display->setCursor(11, 10);
+  display->print(F("Let's play a game"));
+  display->setCursor(13, 56);
+  display->print(F("Press any button"));
+}
