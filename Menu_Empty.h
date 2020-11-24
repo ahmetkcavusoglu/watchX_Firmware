c64bf33085f07520dd007924f75cd9dbac216751
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-24 11:03:15 +0300

    Update Menu_Empty.h

--- Menu_Empty.h
+++ Menu_Empty.h
@@ -2,30 +2,20 @@
 #define MENU_EMPTY_H
 
 #include <Arduino.h>
-#include <RTClib.h>
 #include "Menu.h"
 
 
-enum FACE {
-  FACE_GYRO,
-
-  
-  FACE_MAX
-};
 
 class EmptyMenu : public Menu {
 public:
   EmptyMenu();
-  ~EmptyMenu();
 
   bool update();
   void draw(Adafruit_GFX* display) const;
   void button1();
   void button2();
   void button3();
-private:
-  void changeMenu();
-  uint8_t faceType;
+
 };
 
 #endif
