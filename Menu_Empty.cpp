7ece3fb84e6d5d93e070973fd1522cc6f6792e97
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-24 10:34:38 +0300

    Update and rename Menu_Clockface.cpp to Menu_Empty.cpp

---
+++ Menu_Empty.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <RTClib.h>
+#include <Adafruit_GFX.h>
+#include "Menu_Empty.h"
+#include "Menu.h"
+#include "State.h"
+
+EmptyMenu::EmptyMenu()
+  : Menu(MENU_EMPTY)
+
+{}
+
+EmptyMenu::~EmptyMenu() {
+}
+
+bool EmptyMenu::update() {
+  return true;
+}
+
+
+void EmptyMenu::draw(Adafruit_GFX* display) const {
+}
+
+void EmptyMenu::button1() {
+  switchMenu(MENU_PACMAN);
+}
+
+void EmptyMenu::button2() {
+  switchMenu(MENU_PACMAN);
+}
+
+void EmptyMenu::button3() {
+  switchMenu(MENU_PACMAN);
+}
+
+void EmptyMenu::changeMenu() {
+  
+  switch (faceType) {
+    case FACE_GYRO:
+      break;
+  }
+
+}
