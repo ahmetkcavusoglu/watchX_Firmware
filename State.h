01d0c0805b2dbe9030f4a0979c51b0d00cc6d320
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-24 10:40:14 +0300

    Update State.h

--- State.h
+++ State.h
@@ -3,18 +3,13 @@
 
 #include <RTClib.h>
 
-/*
-   A global object holding state and settings.
-*/
 struct State {
   State();
 
   void update();
   DateTime now;
 
-//  // Settings
   uint8_t current_face;
-//  uint8_t enabled_faces;
 
 };
 
