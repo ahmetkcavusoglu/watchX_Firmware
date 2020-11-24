7ee8f43997caa2b1efc96d23743e2d7d6948ab7e
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-24 10:39:38 +0300

    Update State.cpp

--- State.cpp
+++ State.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <EEPROM.h>
 #include "State.h"
 
 State::State()
-//: enabled_faces(0xFF)
 
-{
-//    byte check = EEPROM.read(0);
-//    if (check == 0x43) {
-//    enabled_faces = EEPROM.read(3);
-//
-//  }
-  }
+
+{}
 
 void State::update() {
 }
