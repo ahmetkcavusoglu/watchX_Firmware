f324be70af478bb144eccdf00dac8b83a55cbd9f
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-24 10:33:57 +0300

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include "Menu.h"
-#include "Menu_1.h"
-#include "Menu_2.h"
+#include "Menu_Hello.h"
+#include "Menu_Gyro.h"
 #include "Menu_PressStart.h"
-#include "Menu_5.h"
-#include "Menu_3.h"
-#include "Menu_4.h"
-#include "Menu_Clockface.h"
+#include "Menu_Long.h"
+#include "Menu_Free.h"
+#include "Menu_Lets.h"
+#include "Menu_Empty.h"
 #include "State.h"
-#include "Clockface_Pacman.h"
+#include "Menu_Pacman.h"
 
 extern Menu* menu;
 
@@ -30,50 +30,50 @@ Menu_selection nextMenu;
 bool Menu::update() {
   switch (type) {
     case MENU_START:
-      return ((SettingsTimeMenu*)this)->update();
-    case MENU_CANAVAR:
-      return ((Settings24hMenu*)this)->update();
+      return ((StartMenu*)this)->update();
+    case MENU_GYRO:
+      return ((GyroMenu*)this)->update();
     case MENU_HELLO:
-      return ((SettingsMenu*)this)->update();
-    case MENU_CLOCK:
-      return ((ClockfaceMenu*)this)->update();
-    case MENU_AT:
-      return ((SettingsDateMenu*)this)->update();
-    case MENU_SETTINGS_CLOCKFACE:
-      return ((SettingsClockfaceMenu*)this)->update();
+      return ((HelloMenu*)this)->update();
+    case MENU_EMPTY:
+      return ((EmptyMenu*)this)->update();
+    case MENU_FREE:
+      return ((FreeMenu*)this)->update();
+    case MENU_LETS:
+      return ((LetsMenu*)this)->update();
     case MENU_PACMAN:
       return ((ClockfacePacman*)this)->update();
-    case MENU_5:
-      return ((Menu5*)this)->update();
+    case MENU_LONG:
+      return ((LongMenu*)this)->update();
   }
 }
 
 void Menu::draw(Adafruit_GFX* display) const {
   switch (type) {
     case MENU_START:
-      ((SettingsTimeMenu*)this)->draw(display);
+      ((StartMenu*)this)->draw(display);
       return;
     case MENU_HELLO:
-      ((SettingsMenu*)this)->draw(display);
+      ((HelloMenu*)this)->draw(display);
       return;
-    case MENU_CANAVAR:
-      ((Settings24hMenu*)this)->draw(display);
+    case MENU_GYRO:
+      ((GyroMenu*)this)->draw(display);
       return;
 
-    case MENU_AT:
-      ((SettingsDateMenu*)this)->draw(display);
+    case MENU_FREE:
+      ((FreeMenu*)this)->draw(display);
       return;
-    case MENU_CLOCK:
-      ((ClockfaceMenu*)this)->draw(display);
+    case MENU_EMPTY:
+      ((EmptyMenu*)this)->draw(display);
       return;
-    case MENU_SETTINGS_CLOCKFACE:
-      ((SettingsClockfaceMenu*)this)->draw(display);
+    case MENU_LETS:
+      ((LetsMenu*)this)->draw(display);
       return;
     case MENU_PACMAN:
       ((ClockfacePacman*)this)->draw(display);
       return;
-    case MENU_5:
-      ((Menu5*)this)->draw(display);
+    case MENU_LONG:
+      ((LongMenu*)this)->draw(display);
       return;
 
   }
@@ -82,63 +82,63 @@ void Menu::draw(Adafruit_GFX* display) const {
 void Menu::button1() {
   switch (type) {
     case MENU_HELLO:
-      return ((SettingsMenu*)this)->button1();
-    case MENU_CANAVAR:
-      return ((Settings24hMenu*)this)->button1();
+      return ((HelloMenu*)this)->button1();
+    case MENU_GYRO:
+      return ((GyroMenu*)this)->button1();
     case MENU_START:
-      return ((SettingsTimeMenu*)this)->button1();
-    case MENU_CLOCK:
-      return ((ClockfaceMenu*)this)->button1();
-    case MENU_AT:
-      return ((SettingsDateMenu*)this)->button1();
-    case MENU_SETTINGS_CLOCKFACE:
-      return ((SettingsClockfaceMenu*)this)->button1();
+      return ((StartMenu*)this)->button1();
+    case MENU_EMPTY:
+      return ((EmptyMenu*)this)->button1();
+    case MENU_FREE:
+      return ((FreeMenu*)this)->button1();
+    case MENU_LETS:
+      return ((LetsMenu*)this)->button1();
     case MENU_PACMAN:
       return ((ClockfacePacman*)this)->button1();
-    case MENU_5:
-      return ((Menu5*)this)->button1();
+    case MENU_LONG:
+      return ((LongMenu*)this)->button1();
   }
 }
 
 void Menu::button3() {
   switch (type) {
     case MENU_HELLO:
-      return ((SettingsMenu*)this)->button3();
-    case MENU_CANAVAR:
-      return ((Settings24hMenu*)this)->button3();
+      return ((HelloMenu*)this)->button3();
+    case MENU_GYRO:
+      return ((GyroMenu*)this)->button3();
     case MENU_START:
-      return ((SettingsTimeMenu*)this)->button3();
-    case MENU_CLOCK:
-      return ((ClockfaceMenu*)this)->button3();
-    case MENU_AT:
-      return ((SettingsDateMenu*)this)->button3();
-    case MENU_SETTINGS_CLOCKFACE:
-      return ((SettingsClockfaceMenu*)this)->button3();
+      return ((StartMenu*)this)->button3();
+    case MENU_EMPTY:
+      return ((EmptyMenu*)this)->button3();
+    case MENU_FREE:
+      return ((FreeMenu*)this)->button3();
+    case MENU_LETS:
+      return ((LetsMenu*)this)->button3();
     case MENU_PACMAN:
       return ((ClockfacePacman*)this)->button3();
-    case MENU_5:
-      return ((Menu5*)this)->button3();
+    case MENU_LONG:
+      return ((LongMenu*)this)->button3();
   }
 }
 
 void Menu::button2() {
   switch (type) {
     case MENU_HELLO:
-      return ((SettingsMenu*)this)->button2();
-    case MENU_CANAVAR:
-      return ((Settings24hMenu*)this)->button2();
+      return ((HelloMenu*)this)->button2();
+    case MENU_GYRO:
+      return ((GyroMenu*)this)->button2();
     case MENU_START:
-      return ((SettingsTimeMenu*)this)->button2();
-    case MENU_CLOCK:
-      return ((ClockfaceMenu*)this)->button2();
-    case MENU_AT:
-      return ((SettingsDateMenu*)this)->button2();
-    case MENU_SETTINGS_CLOCKFACE:
-      return ((SettingsClockfaceMenu*)this)->button2();
+      return ((StartMenu*)this)->button2();
+    case MENU_EMPTY:
+      return ((EmptyMenu*)this)->button2();
+    case MENU_FREE:
+      return ((FreeMenu*)this)->button2();
+    case MENU_LETS:
+      return ((LetsMenu*)this)->button2();
     case MENU_PACMAN:
       return ((ClockfacePacman*)this)->button2();
-    case MENU_5:
-      return ((Menu5*)this)->button2();
+    case MENU_LONG:
+      return ((LongMenu*)this)->button2();
   }
 }
 
@@ -157,8 +157,8 @@ void updateMenuSelection() {
   // Okay we need to change. delete the old one.
   if (menu) {
     // If the menu needs a destructor, you need to do this.
-    if (menu->getType() == MENU_CLOCK) {
-      delete (ClockfaceMenu*)menu;
+    if (menu->getType() == MENU_EMPTY) {
+      delete (EmptyMenu*)menu;
     } else {
       delete menu;
     }
@@ -168,28 +168,28 @@ void updateMenuSelection() {
   // Make the new one.
   switch (nextMenu) {
     case MENU_HELLO:
-      menu = new SettingsMenu();
+      menu = new HelloMenu();
       break;
-    case MENU_CANAVAR:
-      menu = new Settings24hMenu();
+    case MENU_GYRO:
+      menu = new GyroMenu();
       break;
     case MENU_START:
-      menu = new SettingsTimeMenu();
+      menu = new StartMenu();
       break;
-    case MENU_CLOCK:
-      menu = new ClockfaceMenu();
+    case MENU_EMPTY:
+      menu = new EmptyMenu();
       break;
-    case MENU_AT:
-      menu = new SettingsDateMenu();
+    case MENU_FREE:
+      menu = new FreeMenu();
       break;
-    case MENU_SETTINGS_CLOCKFACE:
-      menu = new SettingsClockfaceMenu();
+    case MENU_LETS:
+      menu = new LetsMenu();
       break;
     case MENU_PACMAN:
       menu = new ClockfacePacman();
       break;
-       case MENU_5:
-      menu = new Menu5();
+       case MENU_LONG:
+      menu = new LongMenu();
       break;
   }
 }
