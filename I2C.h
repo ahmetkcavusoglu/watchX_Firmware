95a73fadbf35e8653a2b9ee805d1a96e5997a5b3
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-25 12:14:40 +0300

    Update I2C.h

--- I2C.h
+++ I2C.h
@@ -1,5 +1,4 @@
 const uint8_t IMUAddress = 0x69; // AD0 is logic low on the PCB
-const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
 
 uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
   Wire.beginTransmission(IMUAddress);
@@ -16,7 +15,7 @@ uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
 }
 
 uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
-  uint32_t timeOutTimer;
+//  uint32_t timeOutTimer;
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
   uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
@@ -28,8 +27,8 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
     if (Wire.available())
       data[i] = Wire.read();
     else {
-      timeOutTimer = micros();
-      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+//      timeOutTimer = micros();
+//      while (((micros() - timeOutTimer) < 1000) && !Wire.available());
       if (Wire.available())
         data[i] = Wire.read();
       else {
@@ -38,4 +37,5 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
     }
   }
   return 0; // Success
+
 }
